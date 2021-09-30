f60def102a6b6ff4382ca39a7b408743ba7fb2bd
Octavio Lafourcade <octaviolafourcade@MacBook-Pro-de-Octavio.local>
2021-09-29 22:42:27 -0500

    [ UPDATE ] Implement validations

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -10,31 +10,36 @@ class Movies extends React.Component {
 
     };
     render() { 
-        return <div>
-            <table className="table">
-                <thead>
-                    <tr>
-                        <th>Title</th>
-                        <th>Genre</th>
-                        <th>Stock</th>
-                        <th>Rate</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                { this.state.movies.map(movie => (
-                    <tr key={movie._id}>
-                        <td>{ movie.title }</td>
-                        <td>{ movie.genre.name }</td>
-                        <td>{ movie.numberInStock }</td>
-                        <td>{ movie.dailyRentalRate }</td>
-                        <td><button onClick={() => this.handleDelete(movie)} className="btn btn-danger btn-sm">Delete</button></td>
-                    </tr>
-                    ))}
-                    
-                </tbody>
-            </table>
-        </div>;
+        const { length: count } = this.state.movies;
+        if(count === 0) 
+            return <p>There are no movies in the database</p>
+        return (
+            <div>
+                <p>Showing {count}</p>
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th>Title</th>
+                                <th>Genre</th>
+                                <th>Stock</th>
+                                <th>Rate</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        { this.state.movies.map(movie => (
+                            <tr key={movie._id}>
+                                <td>{ movie.title }</td>
+                                <td>{ movie.genre.name }</td>
+                                <td>{ movie.numberInStock }</td>
+                                <td>{ movie.dailyRentalRate }</td>
+                                <td><button onClick={() => this.handleDelete(movie)} className="btn btn-danger btn-sm">Delete</button></td>
+                            </tr>
+                            ))}
+                            
+                        </tbody>
+                    </table>
+            </div>);
     }
 }
  
